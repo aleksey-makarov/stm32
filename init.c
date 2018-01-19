eb00d861a5ca248400745b12dae58229f8a4278b
Aleksey Makarov <aleksey.makarov@cavium.com>
2018-01-19 11:16:02 +0600

    wip

--- init.c
+++ init.c
@@ -127,7 +127,7 @@ uint32_t (* const vectortable[]) __attribute__ ((section(".isrvectors"))) = {
   ISR_NOT_IMPL                      /* OTG_FS            0x014C   67    83 */
 };
 
-static void gpio_setup(struct GPIO *gpio, unsigned int bit, uint32_t flags)
+static void gpio_setup(struct GPIO *gpio, unsigned int bit, uint32_t mode)
 {
 	volatile uint32_t *cr;
 	uint32_t v;
@@ -140,8 +140,8 @@ static void gpio_setup(struct GPIO *gpio, unsigned int bit, uint32_t flags)
 	}
 
 	v = *cr;
-	v &= 0xf << (bit * 4);
-	v |= (0xf & flags) << (bit * 4);
+	v &= ~(0xf << (bit * 4));
+	v |= (0xf & mode) << (bit * 4);
 	*cr = v;
 }
 
@@ -160,7 +160,6 @@ static void gpio_setup(struct GPIO *gpio, unsigned int bit, uint32_t flags)
 #define RCC_CR_PLL3ON		(1 << 28)
 #define RCC_CR_PLL3RDY		(1 << 29)
 
-
 #define RCC_CFGR_SW_SHIFT	0
 #define  RCC_CFGR_SW_HSI	(0 << RCC_CFGR_SW_SHIFT)
 #define  RCC_CFGR_SW_HSE	(1 << RCC_CFGR_SW_SHIFT)
@@ -174,9 +173,42 @@ static void gpio_setup(struct GPIO *gpio, unsigned int bit, uint32_t flags)
 #define RCC_CFGR_PLL_SRC	(1 << 16)
 #define RCC_CFGR_PLL_XTPRE	(1 << 17)
 #define RCC_CFGR_PLLMUL_SHIFT	18
-#define  RCC_CFGR_PLLMUL_x9	(0xf << RCC_CFGR_PLLMUL_SHIFT)
+#define  RCC_CFGR_PLLMUL_x9	(7 << RCC_CFGR_PLLMUL_SHIFT)
 #define RCC_CFGR_OTGFS_PRE	(1 << 22)
 #define RCC_CFGR_MCO_SHIFT	24
+
+#define RCC_APB2ENR_AFIOEN 	(1 << 0)
+#define RCC_APB2ENR_IOPAEN 	(1 << 2)
+#define RCC_APB2ENR_IOPBEN 	(1 << 3)
+#define RCC_APB2ENR_IOPCEN 	(1 << 4)
+#define RCC_APB2ENR_IOPDEN 	(1 << 5)
+#define RCC_APB2ENR_IOPEEN 	(1 << 6)
+#define RCC_APB2ENR_ADC1EN 	(1 << 9)
+#define RCC_APB2ENR_ADC2EN 	(1 << 10)
+#define RCC_APB2ENR_TIM1EN 	(1 << 11)
+#define RCC_APB2ENR_SPI1EN 	(1 << 12)
+#define RCC_APB2ENR_USART1EN 	(1 << 14)
+
+#define RCC_APB1ENR_TIM2EN 	(1 << 0)
+#define RCC_APB1ENR_TIM3EN 	(1 << 1)
+#define RCC_APB1ENR_TIM4EN 	(1 << 2)
+#define RCC_APB1ENR_TIM5EN 	(1 << 3)
+#define RCC_APB1ENR_TIM6EN 	(1 << 4)
+#define RCC_APB1ENR_TIM7EN 	(1 << 5)
+#define RCC_APB1ENR_WWDGEN 	(1 << 11)
+#define RCC_APB1ENR_SPI2EN 	(1 << 14)
+#define RCC_APB1ENR_SPI3EN 	(1 << 15)
+#define RCC_APB1ENR_USART2EN 	(1 << 17)
+#define RCC_APB1ENR_USART3EN 	(1 << 18)
+#define RCC_APB1ENR_UART4EN 	(1 << 19)
+#define RCC_APB1ENR_UART5EN 	(1 << 20)
+#define RCC_APB1ENR_I2C1EN 	(1 << 21)
+#define RCC_APB1ENR_I2c2EN 	(1 << 22)
+#define RCC_APB1ENR_CAN1EN 	(1 << 25)
+#define RCC_APB1ENR_CAN2EN 	(1 << 26)
+#define RCC_APB1ENR_BKPEN 	(1 << 27)
+#define RCC_APB1ENR_PWREN 	(1 << 28)
+#define RCC_APB1ENR_DACEN 	(1 << 29)
 
 #define FLASH_ACR_LATENCY_SHIFT	0
 #define FLASH_ACR_HLFCYA	(1 << 3)
@@ -191,7 +223,10 @@ static void rcc_init(void)
 		;
 	FLASH->ACR = FLASH_ACR_PRFTBE | (2 << FLASH_ACR_LATENCY_SHIFT);
 	RCC->CFGR = RCC_CFGR_PLL_SRC | RCC_CFGR_PLLMUL_x9 | RCC_CFGR_SW_PLL | RCC_CFGR_PPRE1_div2;
-	RCC->APB2ENR |= (1 << 2) | (1 << 3) | (1 << 4) | (1 << 14);
+	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN | RCC_APB2ENR_IOPAEN |
+			RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN |
+			RCC_APB2ENR_USART1EN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 }
 
 static void uart1_init(void)
@@ -202,7 +237,7 @@ static void uart1_init(void)
 	USART1->CR2 = 0;
 	USART1->CR3 = 0;
 	USART1->GTPR = 0;
-	USART1->BRR = 72000000;
+	USART1->BRR = 72000000 / 115200;
 }
 
 void reset_handler(void) __attribute__ ((noreturn));
